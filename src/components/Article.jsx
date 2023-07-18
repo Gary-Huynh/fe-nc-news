7e8e6dcd23ae5762586cd0e820d52d7173a943b4
Gary-Huynh <gary.c.huynh@gmail.com>
2023-07-18 10:55:46 +0100

    changed input to text area and added auto refreshing and auto display posted comment

--- src/components/Article.jsx
+++ src/components/Article.jsx
@@ -1,16 +1,17 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import { getArticle, postComment } from "../../api";
+import { getArticle,} from "../../api";
 import Comments from "./Comments";
 import { UserContext } from "../contexts/UserContext";
 
 const Article = () =>{
 const {user} = useContext(UserContext)
-const [newComment, setNewComment] = useState({username:`${user}`, body: ""})
+
 const [article, setArticle] = useState()
 const article_id = useParams();
 const [isLoading, setIsLoading] = useState(true)
 const[error, setError] = useState(false)
+
 useEffect(()=>{
     setIsLoading(true)
     setError(true)
@@ -27,15 +28,9 @@ useEffect(()=>{
 
     })
 },[])
-const handleSubmit = (e)=>{
-    e.preventDefault()
 
-    postComment(article_id, newComment).then((returnedComment)=>{
-            return returnedComment
-    })
 
-    setNewComment({username:`${user}`, body:"Comment Posted!"})
-}
+
 
 if(isLoading) {return <h1>Loading now...</h1>}
 if(error){return <h1>Something went wrong try again later 🙄</h1>}
@@ -51,16 +46,7 @@ if(error){return <h1>Something went wrong try again later 🙄</h1>}
         <p>Votes: {article.votes}</p>
         <p>Comment Count:  {article.comment_count}</p>
         <br/> <br/> <br/> <br/>
-        <form onSubmit={handleSubmit}>
-            <label htmlFor="postComment">Write your new comment here!  </label>
-            <input id="postComment" type="text" value={newComment.body}
-            onChange={((e)=>{
-                setNewComment((currBody)=>{
-                    return {...currBody, body:e.target.value}
-                })
-            })} required/>
-            <button disabled={newComment.body==="Comment Posted!"}>Post Comment!</button >
-        </form>
+
 
 
         <h3>Comments</h3>
